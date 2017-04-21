4204b5503a77280e77bec45d8c4cd8b7c0ab5e08
William S. Woodruff <williamswoodruff@gmail.com>
2017-04-20 22:07:57 -0400

    Change home buttons to mdl buttons

--- src/scenes/home/components/HomeView.js
+++ src/scenes/home/components/HomeView.js
@@ -2,6 +2,17 @@ const React = require('react');
 const { View, Text, Image, StyleSheet } = require('react-native');
 const Styles = require('./styles');
 
+const { MKButton } = require('react-native-material-kit');
+
+const HomeBtn = MKButton.coloredButton()
+    .withText('Home')
+    .build();
+
+const CounterBtn = MKButton.coloredButton()
+    .withText('Counter')
+    .build();
+
+
 class HomeView extends React.Component {
 
     static propTypes = {
@@ -15,14 +26,15 @@ class HomeView extends React.Component {
         return (
             <View style={{ margin: 128 }}>
 
-                <Text onPress={() => {
+                <HomeBtn onPress={() => {
 
                     navigation.navigate('Home');
-                }}>Home</Text>
-                <Text onPress={() => {
+                }} />
+
+                <CounterBtn onPress={() => {
 
                     navigation.navigate('Counter');
-                }}>Counter</Text>
+                }} />
 
                 <Text style={Styles.title}>Welcome!</Text>
                 <Image
